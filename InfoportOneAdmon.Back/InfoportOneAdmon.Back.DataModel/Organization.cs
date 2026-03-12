ce60e4cbeca70287259b06ef617038e387d8e023
Miguel Albert <malbert@infoport.es>
2026-03-12 23:12:55 +0100

    Error de guardado de modulos en organizaciones

--- InfoportOneAdmon.Back/InfoportOneAdmon.Back.DataModel/Organization.cs
+++ InfoportOneAdmon.Back/InfoportOneAdmon.Back.DataModel/Organization.cs
@@ -5,65 +5,67 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Helix6.Base.Domain.BaseInterfaces;
 // using Microsoft.EntityFrameworkCore;
 
-namespace InfoportOneAdmon.Back.DataModel {
-
-[Table("Organization", Schema = "Admon")]
-// [Index("GroupId", Name = "IX_Organization_GroupId")]
-// [Index("Name", Name = "UX_Organization_Name", IsUnique = true)]
-// [Index("SecurityCompanyId", Name = "UX_Organization_SecurityCompanyId", IsUnique = true)]
-// [Index("TaxId", Name = "UX_Organization_TaxId", IsUnique = true)]
-public partial class Organization : IEntityBase
+namespace InfoportOneAdmon.Back.DataModel
 {
-    [Key]
-    public int Id { get; set; }
 
-    public int SecurityCompanyId { get; set; }
+    [Table("Organization", Schema = "Admon")]
+    // [Index("GroupId", Name = "IX_Organization_GroupId")]
+    // [Index("Name", Name = "UX_Organization_Name", IsUnique = true)]
+    // [Index("SecurityCompanyId", Name = "UX_Organization_SecurityCompanyId", IsUnique = true)]
+    // [Index("TaxId", Name = "UX_Organization_TaxId", IsUnique = true)]
+    public partial class Organization : IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
 
-    public int? GroupId { get; set; }
+        public int SecurityCompanyId { get; set; }
 
-    [StringLength(200)]
-    public string Name { get; set; }
+        public int? GroupId { get; set; }
 
-    [StringLength(50)]
-    public string Acronym { get; set; }
+        [StringLength(200)]
+        public string Name { get; set; }
 
-    [StringLength(50)]
-    public string TaxId { get; set; }
+        [StringLength(50)]
+        public string Acronym { get; set; }
 
-    [StringLength(300)]
-    public string Address { get; set; }
+        [StringLength(50)]
+        public string TaxId { get; set; }
 
-    [StringLength(100)]
-    public string City { get; set; }
+        [StringLength(300)]
+        public string Address { get; set; }
 
-    [StringLength(20)]
-    public string PostalCode { get; set; }
+        [StringLength(100)]
+        public string City { get; set; }
 
-    [StringLength(100)]
-    public string Country { get; set; }
+        [StringLength(20)]
+        public string PostalCode { get; set; }
 
-    [StringLength(255)]
-    public string ContactEmail { get; set; }
+        [StringLength(100)]
+        public string Country { get; set; }
 
-    [StringLength(50)]
-    public string ContactPhone { get; set; }
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
 
-    [StringLength(255)]
-    public string AuditCreationUser { get; set; }
+        [StringLength(50)]
+        public string ContactPhone { get; set; }
 
-    public DateTime? AuditCreationDate { get; set; }
+        [StringLength(255)]
+        public string AuditCreationUser { get; set; }
 
-    [StringLength(255)]
-    public string AuditModificationUser { get; set; }
+        public DateTime? AuditCreationDate { get; set; }
 
-    public DateTime? AuditModificationDate { get; set; }
+        [StringLength(255)]
+        public string AuditModificationUser { get; set; }
 
-    public DateTime? AuditDeletionDate { get; set; }
+        public DateTime? AuditModificationDate { get; set; }
 
-    [ForeignKey("GroupId")]
-    [InverseProperty("Organization")]
-    public virtual OrganizationGroup Group { get; set; }
+        public DateTime? AuditDeletionDate { get; set; }
 
-    [InverseProperty("Organization")]
-    public virtual ICollection<Organization_ApplicationModule> Organization_ApplicationModule { get; } = new List<Organization_ApplicationModule>();
-}}
+        [ForeignKey("GroupId")]
+        [InverseProperty("Organization")]
+        public virtual OrganizationGroup Group { get; set; }
+
+        [InverseProperty("Organization")]
+        public virtual ICollection<Organization_ApplicationModule> Organization_ApplicationModule { get; set; } = new List<Organization_ApplicationModule>();
+    }
+}
